c793bc9c1c3df9851efbb286acfcb9bb9240624b
efebarandurmaz05@gmail.com <efebarandurmaz05@gmail.com>
2025-02-08 20:33:18 +0300

    Add global styles, navigation, and main sections with responsive design

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import Hero from "./Hero"
+import Specials from "./Specials"
+import Testimonials from "./Testimonials"
+import About from "./About"
 
 const Main = () => {
-    return (
-        <main>
-        </main>
-    );
-};
+  return (
+    <main className="main">
+      <Hero />
+      <Specials />
+      <Testimonials />
+      <About />
+    </main>
+  )
+}
 
-export default Main;
+export default Main
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,39 @@
-import React from 'react';
-
-const Nav = () => {
+function Nav() {
     return (
-        <nav>
-        </nav>
-    );
-};
-
-export default Nav;
+      <nav className="nav">
+        <div className="nav-content">
+          <div className="nav-logo">
+            <img
+              src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Logo-qJyqWG2nEnr2e48MWMgQXkgQTZKNVl.svg"
+              alt="Little Lemon Logo"
+              className="logo"
+            />
+          </div>
+          <ul>
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#menu">Menu</a>
+            </li>
+            <li>
+              <a href="#reservations">Reservations</a>
+            </li>
+            <li>
+              <a href="#order-online">Order Online</a>
+            </li>
+            <li>
+              <a href="#login">Login</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    )
+  }
+  
+  export default Nav
+  
+  
