40642e410ea09140be0f50bd21d68f4d86ca1d03
efebarandurmaz05@gmail.com <efebarandurmaz05@gmail.com>
2025-02-12 23:26:53 +0300

    Add BookingPage and BookingForm components with state management for reservations

---
+++ src/components/BookingForm.js
@@ -0,0 +1,67 @@
+import React from 'react';
+
+const BookingForm = ({
+  resDate,
+  setResDate,
+  resTime,
+  setResTime,
+  guests,
+  setGuests,
+  occasion,
+  setOccasion,
+  availableTimes,
+  handleSubmit
+}) => {
+  return (
+    <>
+      <form onSubmit={handleSubmit} style={{ display: 'grid', maxWidth: '200px', gap: '20px' }}>
+        <label htmlFor="res-date">Choose date</label>
+        <input
+          type="date"
+          id="res-date"
+          value={resDate}
+          onChange={(e) => setResDate(e.target.value)}
+        />
+
+        <label htmlFor="res-time">Choose time</label>
+        <select
+          id="res-time"
+          value={resTime}
+          onChange={(e) => setResTime(e.target.value)}
+        >
+          {availableTimes.map((time) => (
+            <option key={time} value={time}>
+              {time}
+            </option>
+          ))}
+        </select>
+
+        <label htmlFor="guests">Number of guests</label>
+        <input
+          type="number"
+          placeholder="1"
+          min="1"
+          max="10"
+          id="guests"
+          value={guests}
+          onChange={(e) => setGuests(Number(e.target.value))}
+        />
+
+        <label htmlFor="occasion">Occasion</label>
+        <select
+          id="occasion"
+          value={occasion}
+          onChange={(e) => setOccasion(e.target.value)}
+        >
+          <option value="">Select Occasion</option>
+          <option value="Birthday">Birthday</option>
+          <option value="Anniversary">Anniversary</option>
+        </select>
+
+        <input type="submit" value="Make Your reservation" />
+      </form>
+    </>
+  );
+};
+
+export default BookingForm;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,26 @@
+import React, { useReducer } from 'react'
 import Hero from "./Hero"
 import Specials from "./Specials"
 import Testimonials from "./Testimonials"
 import About from "./About"
 import Footer from "./Footer"
+import BookingPage  from "./BookingPage"
+
+// Reducer for available times
+const initialTimes = []  // or your default times array
+const availableTimesReducer = (state, action) => {
+  switch(action.type){
+    case 'update':
+      return action.times
+    default:
+      return state
+  }
+}
 
 const Main = () => {
+  // Lifted up availableTimes state using useReducer
+  const [availableTimes, dispatch] = useReducer(availableTimesReducer, initialTimes)
+
   return (
     <main className="main">
       <Hero />
@@ -12,6 +28,8 @@ const Main = () => {
       <Testimonials />
       <About />
       <Footer />
+      {/* Pass state and dispatch down to BookingPage */}
+      <BookingPage availableTimes={availableTimes} dispatch={dispatch} />
     </main>
   )
 }
